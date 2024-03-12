b568356a11e155c7699435e3d657192ad41e3804
Deepa <nikadedeepa01@gmail.com>
2024-03-12 20:48:06 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Cart from './Components/Cart';
+import { useState } from 'react';
 
 function App() {
+  const [products,setproducts]=useState([
+    {
+      id:1,
+      img:"https://www.course-api.com/images/cart/phone-1.png",
+      name:"Samsung S21",
+      quantity:1,
+      price:100000,
+    },
+    {
+      id:2,
+      img:"https://www.course-api.com/images/cart/phone-2.png",
+      name:"BlackBarry",
+      quantity:1,
+      price:50000,
+    },
+    {
+      id:3,
+      img:"https://www.course-api.com/images/cart/phone-3.png",
+      name:"",
+      quantity:1,
+      price:130000,
+    },
+  ]);
+
+const [quantity,setQuantity]=useState(3);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar quantity={quantity}/>
+      <Cart products={products} setproducts={setproducts} setQuantity={setQuantity} quantity={quantity}/>
     </div>
   );
 }
